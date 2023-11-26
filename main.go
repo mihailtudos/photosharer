ed1b080f8c45b1953a45a3e1b41c6f9d82591fce
Mihail Tudos <mihailtudos@outlook.com>
2023-11-26 18:13:37 +0200

    added hotrelaod

--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
+func home(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "<h1>Hello to my awesome site</h1>")
+}
+
+func main() {
+	http.HandleFunc("/", home)
+	fmt.Println("starting server at 8080...")
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
