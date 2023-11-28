b5c07753038981c79d20fadde7cf4c59d79ffb6d
Mihail Tudos <mihailtudos@outlook.com>
2023-11-28 14:53:59 +0200

    added 3rd party router

--- main.go
+++ main.go
@@ -2,16 +2,51 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi/v5"
 	"log"
 	"net/http"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
+type Router struct{}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Hello to my awesome site</h1>")
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL.Path)
+	fmt.Fprintln(w, "<h1>Welcome to the contact page</h1>")
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.Method, r.URL.Path)
+	fmt.Fprintln(w, "<h1>Welcome to the FAQ page</h1>")
+}
+
+func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+
+	return
+}
+
 func main() {
-	http.HandleFunc("/", home)
+	r := chi.NewRouter()
+	r.Get("/", homeHandler)
+	r.Get("/contact", contactHandler)
+	r.Get("/faq", faqHandler)
+	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
+		http.NotFound(w, r)
+	})
+
 	fmt.Println("starting server at 8080...")
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 }
