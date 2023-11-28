040ab2bdfd91d2d5740677fe8b125a5e94cfa28f
Mihail Tudos <mihailtudos@outlook.com>
2023-11-28 15:07:38 +0200

    added basic middleware and rout with params

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/go-chi/chi/v5"
 	"log"
@@ -23,29 +24,34 @@ func faqHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintln(w, "<h1>Welcome to the FAQ page</h1>")
 }
 
-func (Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	switch r.URL.Path {
-	case "/":
-		homeHandler(w, r)
-	case "/contact":
-		contactHandler(w, r)
-	case "/faq":
-		faqHandler(w, r)
-	default:
-		http.NotFound(w, r)
-	}
+func testHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	user := r.Context().Value("user")
 
-	return
+	fmt.Fprintln(w, fmt.Sprintf("<h1>Showing artile with id %s and user is %s</h1>", id, user))
+}
+
+func MyMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.WithValue(r.Context(), "user", "123")
+
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
 
 func main() {
 	r := chi.NewRouter()
+
+	r.Use(MyMiddleware)
 	r.Get("/", homeHandler)
+
 	r.Get("/contact", contactHandler)
 	r.Get("/faq", faqHandler)
 	r.NotFound(func(w http.ResponseWriter, r *http.Request) {
 		http.NotFound(w, r)
 	})
+
+	r.Get("/article/{id:[0-9]+}", testHandler)
 
 	fmt.Println("starting server at 8080...")
 	log.Fatal(http.ListenAndServe("localhost:8080", r))
