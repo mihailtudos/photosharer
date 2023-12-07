11aff3dea55a7a70e2d28542d0e12bfe1acda418
Mihail Tudos <mihailtudos@outlook.com>
2023-12-07 16:53:59 +0200

    completed password token generator method

--- models/password_reset.go
+++ models/password_reset.go
@@ -1,14 +1,16 @@
 package models
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/base64"
 	"fmt"
+	"github.com/mihailtudos/photosharer/rand"
+	"strings"
 	"time"
 )
 
 const (
-	// MinBytesPerToken - the min number of bytes to be used for each session token.
-	// MinBytesPerToken     = 32
 	DefaultResetDuration = 1 * time.Hour
 )
 
@@ -33,9 +35,60 @@ type PasswordResetService struct {
 }
 
 func (ps *PasswordResetService) Create(email string) (*PasswordReset, error) {
-	return nil, fmt.Errorf("TODO: implement password reeset service")
+	email = strings.ToLower(email)
+	var userId int
+	row := ps.DB.QueryRow(`
+		SELECT id FROM users WHERE email = $1;
+	`, email)
+
+	err := row.Scan(&userId)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	bytesPerToken := ps.BytesPerToken
+	if bytesPerToken < MinBytesPerToken {
+		bytesPerToken = MinBytesPerToken
+	}
+
+	token, err := rand.String(bytesPerToken)
+
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	duration := ps.Duration
+	if duration == 0 {
+		duration = DefaultResetDuration
+	}
+
+	pwReset := PasswordReset{
+		UserID:    userId,
+		Token:     token,
+		TokenHash: ps.hash(token),
+		ExpiresAt: time.Now().Add(duration),
+	}
+
+	row = ps.DB.QueryRow(`
+		INSERT INTO password_resets (user_id, token_hash, expires_at)
+		VALUES ($1, $2, $3) ON CONFLICT (user_id) DO 
+		    UPDATE 
+		SET token_hash = $2, expires_at = $3 
+		RETURNING id;`, pwReset.UserID, pwReset.TokenHash, pwReset.ExpiresAt)
+
+	err = row.Scan(&pwReset.ID)
+	if err != nil {
+		return nil, fmt.Errorf("create: %w", err)
+	}
+
+	return &pwReset, nil
 }
 
 func (ps *PasswordResetService) Consume(token string) (*User, error) {
 	return nil, fmt.Errorf("TODO: implement password reset consume ")
+}
+
+func (ps *PasswordResetService) hash(token string) string {
+	tokenHash := sha256.Sum256([]byte(token))
+	return base64.URLEncoding.EncodeToString(tokenHash[:])
 }
